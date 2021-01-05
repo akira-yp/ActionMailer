7a9fdaf1bc57c7fd873efab64e10ae250eec0161
akira-yp <yog.puripuri@gmail.com>
2021-01-05 16:21:39 +0900

    production.rbを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,9 +98,9 @@ Rails.application.configure do
     user_name:ENV['SENDGRID_USERNAME'],
     password:ENV['SENDGRID_PASSWORD'],
     domain:'heroku.com',
-    adress:'smtp.SendGrid.net',
-    port:'587',
-    authentication:'plain',
-    enable_starttls_auto:true
+    address:'smtp.SendGrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
