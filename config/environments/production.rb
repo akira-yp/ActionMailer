a9b59b21290d78ecdaabfd9a944d33bcc3ef20ff
akira-yp <yog.puripuri@gmail.com>
2021-01-05 14:48:32 +0900

    SendGridの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = [host:'lit-stream-98652.herokuapp.com/']
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name:ENV['SENDGRID_USERNAME'],
+    password:ENV['SENDGRID_PASSWORD'],
+    domain:'heroku.com',
+    adress:'smtp.SendGrid.net',
+    port:'587',
+    authentication:'plain',
+    enable_starttle_auto:true
+  }
 end
